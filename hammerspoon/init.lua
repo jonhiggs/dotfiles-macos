58a718523ae9801fbcd26e3a8ec337f3a887d9fc
Jon Higgs <jhiggs@eml.cc>
2020-07-10 13:10:55 +1000

    Adjust media keys

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -87,14 +87,15 @@ require "window_management"
 
 -- cmus
 hs.task = require("hs.task")
-hs.hotkey.bind({}, "F14", function()
+hs.hotkey.bind({}, "F13", function()
   hs.task.new("/Users/jonhiggs/.local/src/dotfiles-macos/bash/bin/cmus-remote-remote", nil, {"--pause"}):start()
+end)
+
+hs.hotkey.bind({}, "F14", function()
+  hs.task.new("/Users/jonhiggs/.local/src/dotfiles-macos/bash/bin/cmus-remote-remote", nil, {"--prev"}):start()
 end)
 
 hs.hotkey.bind({}, "F15", function()
   hs.task.new("/Users/jonhiggs/.local/src/dotfiles-macos/bash/bin/cmus-remote-remote", nil, {"--next"}):start()
 end)
 
-hs.hotkey.bind({}, "F13", function()
-  hs.task.new("/Users/jonhiggs/.local/src/dotfiles-macos/bash/bin/cmus-remote-remote", nil, {"--prev"}):start()
-end)
