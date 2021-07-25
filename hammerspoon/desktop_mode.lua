e3164424fefba64000fa5a355c1fb2825bb0bd1a
Jon Higgs <jhiggs@eml.cc>
2021-07-26 09:09:44 +1000

    handle desktop mode

--- hammerspoon/desktop_mode.lua
+++ hammerspoon/desktop_mode.lua
@@ -1,9 +1,31 @@
 local logger = hs.logger.new("init", "info")
+logger.i("desktop mode is loaded")
 
--- togggle desktop mode
-hs.hotkey.bind({"cmd", "shift"}, "return", function()
-	logger.i("toggle desktop mode")
-	interfaces = hs.network.interfaces()
-	logger.i(table.concat(interfaces,","))
-	logger.i(table.concat(hs.network.interfaceDetails("en0"),","))
+function is_h0002(data)
+	return (data["vendorID"] == 65261) and (data["productID"] == 0)
+end
+
+function is_lan(data)
+	return (data["vendorID"] == 3034) and (data["productID"] == 33107)
+end
+
+usbWatcher = hs.usb.watcher.new(function(data)
+	if is_h0002(data) and data["eventType"] == "added" then
+		logger.i("found a keyboard")
+	end
+
+	if is_lan(data) and data["eventType"] == "added" then
+		logger.i("enabling bluetooth")
+		hs.task.new("/usr/local/bin/blueutil", nil, {"-p1"}):start()
+
+		logger.i("disabling wifi")
+		hs.task.new("/usr/sbin/networksetup", nil, {"-setairportpower", "en0", "off"})
+	elseif is_lan(data) and data["eventType"] == "removed" then
+		logger.i("disabling bluetooth")
+		hs.task.new("/usr/local/bin/blueutil", nil, {"-p0"}):start()
+
+		logger.i("enabling wifi")
+		hs.task.new("/usr/sbin/networksetup", nil, {"-setairportpower", "en0", "on"})
+	end
 end)
+usbWatcher:start()
